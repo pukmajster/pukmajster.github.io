fc9de52a13e64702ff4cf97f247a3956c4d4d685
pukmajster <45610204+pukmajster@users.noreply.github.com>
2026-01-17 13:27:38 +0100

    init

---
+++ scripts/project-card.js
@@ -0,0 +1,116 @@
+class CProjectCard extends HTMLElement {
+  constructor() {
+    super();
+
+    const landingPageUrl = this.getAttribute("landing-page-url");
+    const timeframe = this.getAttribute("timeframe");
+    const appUrl = this.getAttribute("app-url");
+    const githubUrl = this.getAttribute("github-url");
+    const title = this.getAttribute("title");
+    const about = this.getAttribute("about");
+    const details = this.getAttribute("details");
+    const stack = this.getAttribute("stack");
+
+    this.innerHTML = `
+       <article class="ProjectCard">
+          <div class="ProjectCardHeader">
+            <h4>${title}</h4>
+            ${timeframe ? `<span class="ProjectCardTimeframe">${timeframe}</span>` : ""}
+          </div>
+
+          <p>${about}</p>
+
+          ${details ? `<p>${details}</p>` : ""}
+
+          <p class="ProjectCardStack">${stack}</p>
+
+          <div class="ProjectCardLinks">
+                   ${appUrl ? `<a class="Link" target="_blank" href="${appUrl}">App</a>` : ""}
+            ${landingPageUrl ? `<a class="Link" target="_blank" href="${landingPageUrl}">Landing page</a>` : ""}
+            ${githubUrl ? `<a class="Link" target="_blank" href="${githubUrl}">GitHub</a>` : ""}
+          </div>
+        </article>
+    `;
+  }
+}
+
+class CProjectCatergory extends HTMLElement {
+  constructor() {
+    super();
+
+    const label = this.getAttribute("label");
+    const padded = !this.getAttribute("no-padding");
+
+    this.innerHTML = `
+      <div class="ProjectCategory ${padded ? "ProjectCategory__Padded" : ""}">
+        <h4 class="TextPadding">${label}</h4>
+        <div class="ProjectCategoryLine"></div>
+      </div>
+    `;
+  }
+}
+
+class CIconLinkedIn extends HTMLElement {
+  constructor() {
+    super();
+
+    this.innerHTML = `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        aria-hidden="true"
+        role="img"
+        class="Icon"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        data-v-6e89ca90=""
+      >
+        <path
+          fill="currentColor"
+          d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037c-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85c3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.06 2.06 0 0 1-2.063-2.065a2.064 2.064 0 1 1 2.063 2.065m1.782 13.019H3.555V9h3.564zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0z"
+        ></path>
+      </svg>
+    `;
+  }
+}
+
+class CIconGitHub extends HTMLElement {
+  constructor() {
+    super();
+
+    this.innerHTML = `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        aria-hidden="true"
+        role="img"
+        class="Icon"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        data-v-6e89ca90=""
+      >
+        <path fill="currentColor" d="M12 .297c-6.63 0-12 5.373-12 12c0 5.303 3.438 9.8 8.205 11.385c.6.113.82-.258.82-.577c0-.285-.01-1.04-.015-2.04c-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729c1.205.084 1.838 1.236 1.838 1.236c1.07 1.835 2.809 1.305 3.495.998c.108-.776.417-1.305.76-1.605c-2.665-.3-5.466-1.332-5.466-5.93c0-1.31.465-2.38 1.235-3.22c-.135-.303-.54-1.523.105-3.176c0 0 1.005-.322 3.3 1.23c.96-.267 1.98-.399 3-.405c1.02.006 2.04.138 3 .405c2.28-1.552 3.285-1.23 3.285-1.23c.645 1.653.24 2.873.12 3.176c.765.84 1.23 1.91 1.23 3.22c0 4.61-2.805 5.625-5.475 5.92c.42.36.81 1.096.81 2.22c0 1.606-.015 2.896-.015 3.286c0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path>
+      </svg>
+    `;
+  }
+}
+
+class CHomeButton extends HTMLElement {
+  constructor() {
+    super();
+
+    this.innerHTML = `
+      <a id="HomeButton" href="/">
+        <svg fill="currentColor" viewBox="0 0 52 52" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g data-name="Group 132" id="Group_132"> <path d="M38,52a2,2,0,0,1-1.41-.59l-24-24a2,2,0,0,1,0-2.82l24-24a2,2,0,0,1,2.82,0,2,2,0,0,1,0,2.82L16.83,26,39.41,48.59A2,2,0,0,1,38,52Z"></path> </g> </g></svg>
+      </a>
+    `;
+  }
+}
+
+customElements.define("p-project-card", CProjectCard);
+customElements.define("p-project-category", CProjectCatergory);
+customElements.define("p-icon-linkedin", CIconLinkedIn);
+customElements.define("p-icon-github", CIconGitHub);
+customElements.define("p-home-button", CHomeButton);
